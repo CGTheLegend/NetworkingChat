cd6b6d568c35736c3e781a1f4bf33cecce2c589f
Cristian Gonzalez <cfgonzal@ucsc.edu>
2016-03-09 17:38:58 -0800

    Commented .hs and added another version of java - Commented server.hs - Added a new Java implementation 	- server2.java = server 	- server3.java = server thread - Not 100% on how java code works yet

---
+++ Haskell/server.hs
@@ -0,0 +1,66 @@
+module Main where
+
+import Network.Socket
+import System.IO
+import Control.Exception
+import Control.Concurrent
+import Control.Concurrent.Chan
+import Control.Monad (liftM, when)
+import Control.Monad.Fix (fix)
+
+type Msg = (Int, String)
+
+--- Socket Connection
+main :: IO ()
+main = do
+	sock <- socket AF_INET Stream 0           -- create reusable socket
+	setSocketOption sock ReuseAddr 1
+	bind sock (SockAddrInet 5000 iNADDR_ANY)  -- open TCP connection on port 5000
+	listen sock 2
+	chan <- newChan
+	forkIO $ fix $ \loop -> do
+		(_, msg) <- readChan chan
+		loop
+	mainLoop sock chan 0
+
+--	Accept Connection
+--  Run Server Logic
+mainLoop :: Socket -> Chan Msg -> Int -> IO ()
+mainLoop sock chan msgNum = do
+	conn <- accept sock
+	forkIO (runConn conn chan msgNum)
+	mainLoop sock chan $! msgNum + 1
+
+-- Server Logic
+runConn :: (Socket, SockAddr) -> Chan Msg -> Int -> IO ()
+runConn (sock, _) chan msgNum = do
+	-- setup message, handle and buffer
+	let broadcast msg = writeChan chan (msgNum, msg)
+	hdl <- socketToHandle sock ReadWriteMode
+	hSetBuffering hdl NoBuffering
+
+	-- Prompt user & obtain username
+	hPutStrLn hdl "Hi, what's your name?"
+	name <- liftM init (hGetLine hdl)
+	broadcast ("--> " ++ name ++ " entered chat.")
+	hPutStrLn hdl ("Welcome, " ++ name ++"!")
+
+	-- create new channel available from both locations.
+	commLine <- dupChan chan
+
+	-- fork off a thread for reading the duplicated channel
+	reader <- forkIO $ fix $ \loop -> do
+		(nextNum, line) <- readChan commLine
+		when (msgNum /= nextNum) $ hPutStrLn hdl line
+		loop
+
+	-- handle messages from client
+	handle (\(SomeException _) -> return()) $ fix $ \loop -> do
+		line <- liftM init (hGetLine hdl)
+		case line of
+			"quit" -> hPutStrLn hdl "Bye!"                  -- quit
+			_ -> broadcast (name ++ ": " ++ line) >> loop   -- broadcast message
+
+	killThread reader                                   -- kill thread
+	broadcast ("<--" ++ name ++ " left.")               -- final broadcast
+	hClose hdl                                          -- close handle
